8eb9cfde727ae657458d00b1178c8c4725325286
dogus ural <dogus@riddleandcode.com>
2020-08-10 15:22:20 +0200

    working version of child & parent segregation

--- arbitrager.py
+++ arbitrager.py
@@ -7,42 +7,6 @@ euro_try = float(resp_json['rates']['TRY'])
 print(euro_try)
 
 class exchange:
-     def __init__(self,exchange_url:str):
-        self.exchange_ticker=exchange_url
-        self.bid_prices= dict()
-        self.ask_prices= dict()
-    def refresh(self):
-        response = requests.get(self.exchange_ticker)
-        resp_json = json.loads(response.text)
-        self.get_bid_prices(resp_json)
-        self.get_ask_prices(resp_json)
-
-    def get_btc_ask(self):
-        return  self.ask_prices['BTCTRY']
-    def get_btc_bid(self):
-        return  self.bid_prices['BTCTRY']
-    def get_xtz_ask(self):
-        return  self.ask_prices['XTZTRY']
-    def get_xtz_bid(self):
-        return  self.bid_prices['XTZTRY']
-    def get_link_ask(self):
-        return  self.ask_prices['LINKTRY']
-    def get_link_bid(self):
-        return  self.bid_prices['LINKTRY']
-
-       
-    def get_bid_prices(self,response_json:dict):
-        self.bid_prices['BTCTRY'] = float(response_json['BTC_TL']['highestBid'])
-        self.bid_prices['XTZTRY'] = float(response_json['XTZ_TL']['highestBid'])
-        self.bid_prices['LINKTRY'] = float(response_json['LINK_TL']['highestBid'])
-    def get_ask_prices(self,response_json:dict):
-        self.bid_prices['BTCTRY'] = float(response_json['BTC_TL']['lowestAsk'])
-        self.bid_prices['XTZTRY'] = float(response_json['XTZ_TL']['lowestAsk'])
-        self.bid_prices['LINKTRY'] = float(response_json['LINK_TL']['lowestAsk'])
-
-
-
-class turkish_exchange:
     def __init__(self,exchange_url:str):
         self.exchange_ticker=exchange_url
         self.bid_prices= dict()
@@ -54,6 +18,32 @@ class turkish_exchange:
         self.get_ask_prices(resp_json)
 
     def get_btc_ask(self):
+        pass
+    def get_btc_bid(self):
+        pass
+    def get_xtz_ask(self):
+        pass
+    def get_xtz_bid(self):
+        pass
+    def get_link_ask(self):
+        pass
+    def get_link_bid(self):
+        pass
+
+       
+    def get_bid_prices(self,response_json:dict):
+        pass
+    def get_ask_prices(self,response_json:dict):
+        pass
+
+
+
+class turkish_exchange(exchange):
+    def __init__(self,exchange_url:str):
+        super().__init__(exchange_url)
+    def refresh(self):
+        super().refresh()
+    def get_btc_ask(self):
         return  self.ask_prices['BTCTRY']
     def get_btc_bid(self):
         return  self.bid_prices['BTCTRY']
@@ -76,17 +66,12 @@ class turkish_exchange:
         self.bid_prices['XTZTRY'] = float(response_json['XTZ_TL']['lowestAsk'])
         self.bid_prices['LINKTRY'] = float(response_json['LINK_TL']['lowestAsk'])
 
-class europe_exchange:
+class europe_exchange(exchange):
    
     def __init__(self,exchange_url:str):
-        self.exchange_ticker=exchange_url
-        self.bid_prices= dict()
-        self.ask_prices= dict()
+        super().__init__(exchange_url)
     def refresh(self):
-        response = requests.get(self.exchange_ticker)
-        resp_json = json.loads(response.text)
-        self.get_bid_prices(resp_json)
-        self.get_ask_prices(resp_json)
+        super().refresh()
 
     def get_btc_ask(self):
         return  self.ask_prices['BTCEUR']
