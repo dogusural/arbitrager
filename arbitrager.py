be13b37155cbcd56f4277378a3a4cdaa4bb0d879
dogus ural <dogus@riddleandcode.com>
2020-08-12 14:03:58 +0200

    extended drawer class with an option to redirect the output from stdout to a file

--- arbitrager.py
+++ arbitrager.py
@@ -1,3 +1,4 @@
+import sys
 import euro_oracles,exchanges
 from tabulate import tabulate
 
@@ -16,10 +17,22 @@ class exchange_aggregator:
 
 class console_drawer:
     @staticmethod
-    def draw(text:str):
+    def draw(text:str,file:str=None):
         table = [[text]]
         output = tabulate(table, tablefmt='grid')
-        print(output)
+        console_drawer.smart_print(output,file)
+    @staticmethod
+    def smart_print(text:str,filename:str=None):
+        if filename and filename != '-':
+            fh = open(filename, 'w')
+        else:
+            fh = sys.stdout
+
+        try:
+            print(text, file=fh) # Python 3.x
+        finally:
+            if fh is not sys.stdout:
+                fh.close()
 
 
 class controller:
