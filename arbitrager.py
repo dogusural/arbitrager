fe3d8e27939a21c08ac76c50940dc5c3e33e1a7c
dogus ural <dogus@riddleandcode.com>
2020-08-12 12:24:56 +0200

    better naming to avoid confusions

--- arbitrager.py
+++ arbitrager.py
@@ -23,13 +23,13 @@ class console_drawer:
 
 
 class controller:
-    def __init__(self,exchange_1:str,exchange_2:str):
-        self.exchange_1 = exchange_1
-        self.exchange_2 = exchange_2
+    def __init__(self,try_exchange:str,euro_exchange:str):
+        self.try_exchange = try_exchange
+        self.euro_exchange = euro_exchange
         self.curr_converter = euro_oracles.currconv()
         self.aggregator = exchange_aggregator()
-        self.pair_1 = self.aggregator.get_exchange(exchange_1)
-        self.pair_2 = self.aggregator.get_exchange(exchange_2)
+        self.pair_1 = self.aggregator.get_exchange(try_exchange)
+        self.pair_2 = self.aggregator.get_exchange(euro_exchange)
     def refresh(self):
         self.pair_1.refresh()
         self.pair_2.refresh()
@@ -43,35 +43,35 @@ class controller:
 
 
     def calculate_btc_arbitrage(self):   
-        text = ('Bitcoin bid price on '+ self.exchange_1 +  ' is ' + str(self.pair_1.get_btc_bid())  +
-        '\nBitcoin ask price on '+ self.exchange_2 + ' is ' + str(self.pair_2.get_btc_ask()) +
-        '\nEURO/TRY parity is ' + str(self.curr_converter.get_euro_try_parity()) +'\n'+ self.exchange_2 +
+        text = ('Bitcoin bid price on '+ self.try_exchange +  ' is ' + str(self.pair_1.get_btc_bid())  +
+        '\nBitcoin ask price on '+ self.euro_exchange + ' is ' + str(self.pair_2.get_btc_ask()) +
+        '\nEURO/TRY parity is ' + str(self.curr_converter.get_euro_try_parity()) +'\n'+ self.euro_exchange +
         ' Bitcoin price in TRY is '+ str(self.pair_2.get_btc_ask()) + ' * ' + str(self.curr_converter.get_euro_try_parity()) +
         ' = '+ str(self.pair_2.get_btc_ask()*self.curr_converter.get_euro_try_parity()) +
         '\nArbitrage margin for Bitcoin is ' + str(self.pair_1.get_btc_bid() - self.pair_2.get_btc_ask()*self.curr_converter.get_euro_try_parity()) + ' TRY')
         console_drawer.draw(text)
     def calculate_eth_arbitrage(self):   
-        text = ('Ethereum bid price on '+ self.exchange_1 +  ' is ' + str(self.pair_1.get_eth_bid())  +
-        '\nEthereum ask price on '+ self.exchange_2 + ' is ' + str(self.pair_2.get_eth_ask()) +
-        '\nEURO/TRY parity is ' + str(self.curr_converter.get_euro_try_parity()) +'\n'+ self.exchange_2 +
+        text = ('Ethereum bid price on '+ self.try_exchange +  ' is ' + str(self.pair_1.get_eth_bid())  +
+        '\nEthereum ask price on '+ self.euro_exchange + ' is ' + str(self.pair_2.get_eth_ask()) +
+        '\nEURO/TRY parity is ' + str(self.curr_converter.get_euro_try_parity()) +'\n'+ self.euro_exchange +
         ' Ethereum price in TRY is '+ str(self.pair_2.get_eth_ask()) + ' * ' + str(self.curr_converter.get_euro_try_parity()) +
         ' = '+ str(self.pair_2.get_eth_ask()*self.curr_converter.get_euro_try_parity()) +
         '\nArbitrage margin for Ethereum is ' + str(self.pair_1.get_eth_bid() - self.pair_2.get_eth_ask()*self.curr_converter.get_euro_try_parity()) + ' TRY')
         console_drawer.draw(text)
 
     def calculate_xtz_arbitrage(self):   
-        text = ('Tezos bid price on '+ self.exchange_1 +  ' is ' + str(self.pair_1.get_xtz_bid())  +
-        '\nTezos ask price on '+ self.exchange_2 + ' is ' + str(self.pair_2.get_xtz_ask()) +
-        '\nEURO/TRY parity is ' + str(self.curr_converter.get_euro_try_parity()) +'\n'+ self.exchange_2 +
+        text = ('Tezos bid price on '+ self.try_exchange +  ' is ' + str(self.pair_1.get_xtz_bid())  +
+        '\nTezos ask price on '+ self.euro_exchange + ' is ' + str(self.pair_2.get_xtz_ask()) +
+        '\nEURO/TRY parity is ' + str(self.curr_converter.get_euro_try_parity()) +'\n'+ self.euro_exchange +
         ' Tezos price in TRY is '+ str(self.pair_2.get_xtz_ask()) + ' * ' + str(self.curr_converter.get_euro_try_parity()) +
         ' = '+ str(self.pair_2.get_xtz_ask()*self.curr_converter.get_euro_try_parity()) +
         '\nArbitrage margin for Tezos is ' + str(self.pair_1.get_xtz_bid() - self.pair_2.get_xtz_ask()*self.curr_converter.get_euro_try_parity()) + ' TRY')
         console_drawer.draw(text)
 
     def calculate_link_arbitrage(self):   
-        text = ('Link bid price on '+ self.exchange_1 +  ' is ' + str(self.pair_1.get_link_bid())  +
-        '\nLink ask price on '+ self.exchange_2 + ' is ' + str(self.pair_2.get_link_ask()) +
-        '\nEURO/TRY parity is ' + str(self.curr_converter.get_euro_try_parity()) +'\n'+ self.exchange_2 +
+        text = ('Link bid price on '+ self.try_exchange +  ' is ' + str(self.pair_1.get_link_bid())  +
+        '\nLink ask price on '+ self.euro_exchange + ' is ' + str(self.pair_2.get_link_ask()) +
+        '\nEURO/TRY parity is ' + str(self.curr_converter.get_euro_try_parity()) +'\n'+ self.euro_exchange +
         ' Link price in TRY is '+ str(self.pair_2.get_link_ask()) + ' * ' + str(self.curr_converter.get_euro_try_parity()) +
         ' = '+ str(self.pair_2.get_link_ask()*self.curr_converter.get_euro_try_parity()) +
         '\nArbitrage margin for Link is ' + str(self.pair_1.get_link_bid() - self.pair_2.get_link_ask()*self.curr_converter.get_euro_try_parity()) + ' TRY')
